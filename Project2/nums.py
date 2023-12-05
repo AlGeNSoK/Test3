cd82657851cb958e1b50e8d412d01818b58ad80a
Aleksandr Sokolov <algensok@gmail.com>
2023-12-06 00:21:57 +0300

    Initial commit

---
+++ Project2/nums.py
@@ -0,0 +1,12 @@
+def calc(a, b, op):
+    if op == '+':
+        return a + b
+    elif op == '-':
+        return a - b
+    elif op == '*':
+        return a * b
+    elif op == '/':
+        return a / b
+
+if __name__ == "__main__":
+    print(calc(1, 2, '+'))
